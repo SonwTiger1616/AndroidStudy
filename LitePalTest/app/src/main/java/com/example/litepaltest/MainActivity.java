b68c582e4d7545f34be01e69622a85f522c3f703
SnowTiger1616 <564471087@qq.com>
2017-10-06 21:20:19 +0800

    通过LitePal进行CRUD操作

--- LitePalTest/app/src/main/java/com/example/litepaltest/MainActivity.java
+++ LitePalTest/app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -2,12 +2,91 @@ package com.example.litepaltest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.tablemanager.Connector;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createData = (Button) findViewById(R.id.create_data);
+        createData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Connector.getDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setAuthor("Dan Brown");
+                book.setPages(450);
+                book.setPrice(16.94);
+                book.setName("The Da Vinci Code");
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                // 只能对已存储的对象进行修改，局限性较大
+                Book book = new Book();
+                book.setName("The Lost Symbol");
+                book.setAuthor("Dan Brown");
+                book.setPages(510);
+                book.setPrice(19.95);
+                book.setPress("Unknow");
+                book.save();
+                book.setPrice(10.99);
+                book.save();*/
+
+                // 灵活的修改方式
+                Book book = new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?", "The Lost Symbol", "Dan Brown");
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DataSupport.deleteAll(Book.class, "price < ?", "15");
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Book> bookList = DataSupport.findAll(Book.class);
+                for (Book book : bookList) {
+                    Log.d(TAG, "book name is " + book.getName());
+                    Log.d(TAG, "book author is " + book.getAuthor());
+                    Log.d(TAG, "book price is " + book.getPrice());
+                    Log.d(TAG, "book pages is " + book.getPages());
+                    Log.d(TAG, "book press is " + book.getPress());
+                }
+            }
+        });
+
     }
 }
